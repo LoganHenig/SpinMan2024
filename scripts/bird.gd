dd3a708616659f322afb96febbb06c0b40c84c57
LoganHenig <62856313+LoganHenig@users.noreply.github.com>
2024-07-08 13:58:06 -0400

    added blood for birds
    added blood for birds

--- scripts/bird.gd
+++ scripts/bird.gd
@@ -1,5 +1,7 @@
 extends Node2D
 @onready var animation_bird = $"animation bird"
+@onready var cpu_particles_2d = $CPUParticles2D
 
 func _on_area_2d_area_entered(_area):
 	animation_bird.play("death")
+	cpu_particles_2d.emitting = true
